51adc2c07910b10e02fbc56ff5d3b50b6d5a6e22
Ben Nuttall <ben@bennuttall.com>
2015-06-04 01:34:18 +0000

    Use wait for edge

--- python/garden.py
+++ python/garden.py
@@ -1,26 +1,90 @@
 from __future__ import print_function, division
+from RPi import GPIO
+from dotstar import Adafruit_DotStar
+from time import sleep
 from random import randint
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+PULL = GPIO.PUD_UP
+EDGE = GPIO.FALLING
+BOUNCE = 1000
+
+BUTTON = 18
+
 NUM_LEDS = 100
-POINTS = 10
+DATAPIN = 15
+CLOCKPIN = 14
+
+# colours
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+strip = Adafruit_DotStar(NUM_LEDS, DATAPIN, CLOCKPIN)
+
+def setup_button():
+    """
+    Setup main GPIO button with callback
+    """
+
+    GPIO.setup(BUTTON, GPIO.IN, PULL)
+
+def set_pixel(led, colour):
+    """
+    Set a given pixel on the LED matrix
+    """
+
+    r, g, b = colour
+    strip.setPixelColor(led, r, g, b)
+    strip.show()
+
+def clear_strip(colour):
+    """
+    Clear LED matrix by setting all pixels to a single colour
+    """
+
+    for led in range(NUM_LEDS):
+        set_pixel(led, colour)
 
 def calculate_score():
-    random = randint(0, POINTS)
-    print("random", random)
+    """
+    Calculate score based on scoring algorithm
+    """
 
-    scores = [random]
+    return randint(1, NUM_LEDS)
 
-    total = sum(scores)
-    print("total", total)
-    potential = len(scores) * POINTS
-    print("potential", potential)
-    score = float(total / potential)
-    print("score", score, int(score * NUM_LEDS))
-    return int(score * NUM_LEDS)
+def show_score(score):
+    """
+    Display score on LED matrix
+    """
+
+    for led in range(score):
+        set_pixel(led, GREEN)
+        sleep(0.1)
+
+def callback(pin):
+    print("Run", pin)
+    """
+    Button press callback - calculate score and show on LED matrix
+    """
+
 
 def main():
-    score = calculate_score()
-    print(score)
+    setup_button()
+    strip.begin()
+
+    strip.setBrightness(255)
+    clear_strip(RED)
+    sleep(2)
+
+    print("Ready...")
+
+    while True:
+        GPIO.wait_for_edge(BUTTON, EDGE, BOUNCE)
+        score = calculate_score()
+        print("Score is %s" % score)
+        show_score(score)
 
 if __name__ == '__main__':
     main()
